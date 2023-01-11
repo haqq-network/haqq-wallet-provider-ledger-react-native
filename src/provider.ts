cd9c046e7f1b411d727d01523b5b3d63ded36589
Andrey Makarov <viva.la.akam@gmail.com>
2023-01-11 14:17:14 +0400

    fix: remove transport

--- src/provider.ts
+++ src/provider.ts
@@ -205,7 +205,8 @@ export class ProviderLedgerReactNative extends Provider<ProviderLedgerReactNativ
 
   onDisconnectTransport = () => {
     if (this._transport) {
-      this._transport.off('disconnect', this.onDisconnectTransport)
+      this._transport.off('disconnect', this.onDisconnectTransport);
+      this._transport = null;
     }
   }
 }
